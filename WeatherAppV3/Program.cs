29f1d6fb804d90ebe9093bfa63326d8419159658
pr0s3q <pr0s3q@gmail.com>
2021-10-21 16:43:35 +0200

    Pobieranie danych o pogodzie z konkretnego miasta w formacie JSON za pomoca API

--- WeatherAppV3/Program.cs
+++ WeatherAppV3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 using System.Threading;
 
 namespace WeatherAppV3
@@ -14,7 +16,45 @@ namespace WeatherAppV3
             {
                 Console.WriteLine("Type the city name: ");
                 cityName = Console.ReadLine();
+                CityWeather city = GetCityWeatherInfo(cityName);
             }
+        }
+        static CityWeather GetCityWeatherInfo(string cityName)
+        {
+            string weatherInfo = GetCityWeatherInfoFromAPI(cityName);
+            if (weatherInfo == null)
+            {
+                return null;
+            }
+            else
+            {
+                return CreateCityWeatherObject(weatherInfo);
+            }
+        }
+        static string GetCityWeatherInfoFromAPI(string cityName)
+        {
+            string responseFromServer;
+            WebRequest request = WebRequest.Create("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=873d66abe6afeba78f0f084fe606162d");
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream dataStream = response.GetResponseStream())
+                    {
+                        StreamReader reader = new StreamReader(dataStream);
+                        responseFromServer = reader.ReadToEnd();
+                    }
+                }
+                return responseFromServer;
+            }
+            catch (WebException e)
+            {
+                return null;
+            }
+        }
+        static CityWeather CreateCityWeatherObject(string weatherInfo)
+        {
+
         }
     }
 }
